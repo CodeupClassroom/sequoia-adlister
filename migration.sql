068576d22928f5fe7d325711d905a4b5a8164dd9
stephencisler <stephencisler@gmail.com>
2018-01-10 13:55:17 -0600

    Fixed registration to insert register_date into user_info table when new user is made. migration file altered to remove some 'not-null' statments

--- migration.sql
+++ migration.sql
@@ -5,44 +5,44 @@ CREATE DATABASE adlister_db;
 USE adlister_db;
 
 CREATE TABLE IF NOT EXISTS users (
-id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
-username      VARCHAR(255) NOT NULL,
-email         VARCHAR(255) NOT NULL,
-password      VARCHAR(255) NOT NULL,
-deleted       TINYINT      NOT NULL DEFAULT 0,
-UNIQUE (username),
-UNIQUE (email),
-PRIMARY KEY (id)
+  id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  email    VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  deleted  TINYINT      NOT NULL DEFAULT 0,
+  UNIQUE (username),
+  UNIQUE (email),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_info (
   id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id        INT UNSIGNED NOT NULL,
-  bio           VARCHAR(255) NOT NULL,
-  location      VARCHAR(255) NOT NULL,
+  user_id       INT UNSIGNED NOT NULL,
+  bio           VARCHAR(255),
+  location      VARCHAR(255),
   register_date DATE         NOT NULL,
-  phone_number  VARCHAR(255) NOT NULL,
+  phone_number  VARCHAR(255),
   UNIQUE (user_id),
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS categories (
-id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
-category VARCHAR(255) NOT NULL,
-UNIQUE (category),
-PRIMARY KEY (id)
+  id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  category VARCHAR(255) NOT NULL,
+  UNIQUE (category),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS ads (
-  id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id      INT UNSIGNED NOT NULL,
-  title        VARCHAR(255) NOT NULL,
-  description  TEXT         NOT NULL,
-  date_created DATETIME     NOT NULL,
+  id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id       INT UNSIGNED NOT NULL,
+  title         VARCHAR(255) NOT NULL,
+  description   TEXT         NOT NULL,
+  date_created  DATETIME     NOT NULL,
   date_modified DATETIME,
-  ad_location  VARCHAR(255),
-  deleted      TINYINT      NOT NULL DEFAULT 0,
+  ad_location   VARCHAR(255),
+  deleted       TINYINT      NOT NULL DEFAULT 0,
   PRIMARY KEY (id),
   UNIQUE (user_id, title),
   FOREIGN KEY (user_id) REFERENCES users (id)
