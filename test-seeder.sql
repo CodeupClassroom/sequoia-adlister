7f01d9c15a68a143e0fcdde7714f4d6fa00085a6
stephencisler <stephencisler@gmail.com>
2018-01-10 09:32:55 -0600

    updated seeder

--- test-seeder.sql
+++ test-seeder.sql
@@ -67,14 +67,29 @@ INSERT INTO ads (user_id, title, description, date_created, ad_location) VALUES
    'San Antonio and surrounding areas'),
   (3, 'Free Hugs',
    'Need a hug? Downright sick and tired of not being embraced for a moment? Don\'t wait another second, call me right now and I will be there faster than you can say "Snuggle time!"',
-   NOW(), 'Broadway St, San Antonio');
+   NOW(), 'Broadway St, San Antonio'),
+  (2, 'Need Help Moving Out',
+   'I\'m moving across town and need some movers. Call me for the address and details. One day, $100 for the afternoon',
+   NOW(), 'Austin, TX'),
+  (2, 'Need To Learn to Dance? I\'m the best around!',
+   'Will be doing private lessons at my studio on weekends. If you are interested in styles and session info, call me anytime.',
+   NOW(), 'Austin, TX'),
+  (1, 'Cheap Used Computers',
+   'I have a stock of old school computers I need to get rid of. Compete tower/monitor setups for $100 flat. You must pick up. Various specs, so email if you need something specific',
+   NOW(), 'San Antonio, TX'),
+  (3, 'Coffee Maker For Sale',
+   'Barely used coffee machine, I bought a new one last week I like better. $75 or best offer',
+   NOW(), 'San Antonio, TX'),
+  (1, 'Looking for blank canvases',
+   'looking for blank canvases for painting. can have a little damage or paint on them. size doesn\'t matter',
+   NOW(), 'San Antonio, TX');
 
 SELECT *
 FROM ads;
 
 
 INSERT INTO ads_categories (ads_id, ads_category_id) VALUES
-  (1, 2), (2, 8), (3, 3);
+  (1, 2), (2, 8), (3, 3), (4, 5), (4, 7), (5, 1), (5, 3), (6, 6), (7, 8), (7, 3), (8, 1), (8, 3);
 
 # SELECT to check if ads are being linked to ad_category ids correctly
 SELECT
